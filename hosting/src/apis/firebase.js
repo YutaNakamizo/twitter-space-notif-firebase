3485b95dbf891c2d2178722500371f2af27c9fa6
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2022-02-05 05:40:20 +0900

    #3 excluded Firebase app config json from hosting/src/api/firebase.json

--- hosting/src/apis/firebase.js
+++ hosting/src/apis/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDRFSQh4y65zqAix_8ttEBwWXFLREyZr34",
-  authDomain: "twitter-space-notif.firebaseapp.com",
-  projectId: "twitter-space-notif",
-  storageBucket: "twitter-space-notif.appspot.com",
-  messagingSenderId: "982475283020",
-  appId: "1:982475283020:web:5abec0c5710c53f28ab2e9",
-  measurementId: "G-90CVE5055M"
-};
+import firebaseConfig from './firebaseConfig.json'
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
